9eba90c5ed4861da62ea433372e8f709a616c3f4
Franzé Jr <franzejr@gmail.com>
2017-10-09 12:39:08 -0300

    Get github User Information

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Counter from "./Counter";
 import { observer } from "mobx-react";
 
 @observer
 class App extends Component {
+  componentDidMount(){
+    this.props.appState.getUserInformation();
+  }
   render() {
+    const {name, login, url, blog, location, avatar_url} = this.props.appState.userInformation;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Using Mobx</h1>
         </header>
-        <h3>{this.props.appState.counter}</h3>
-        <Counter increment={this.props.appState.increment} decrement={this.props.appState.decrement} />
+        <img src={avatar_url} />
+        <h1>{name}</h1>
+        <h2>{login}</h2>
+        <h3>{location}</h3>
+        <a href={blog}>Blog</a>
       </div>
     );
   }
--- src/state.js
+++ src/state.js
@@ -1,12 +1,23 @@
 import { observable, action } from "mobx";
+import {create} from "apisauce";
+
+
+const api = create({
+  baseURL: "https://api.github.com"
+})
+
 
 var appState = observable({
   counter: 0,
+  userInformation: {},
   increment: action.bound(function() {
     this.counter++;
   }),
   decrement: action.bound(function(){
     this.counter--;
+  }),
+  getUserInformation: action.bound(function(){
+    api.get("/users/knewter").then(response =>  { this.userInformation = response.data })
   })
 });
 
